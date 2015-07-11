e8ac491fe795f4526e78334d73dfdcc6758bd38f
Mark Mulder <markmulder@gmail.com>
2015-07-11 22:17:34 +0200

    Serve frontend code via HTTP on /

--- cmd/glimpse-server/tailer_server.go
+++ cmd/glimpse-server/tailer_server.go
@@ -11,10 +11,12 @@ import (
 	"time"
 )
 
+// Channel that is used to send messages to the SSE broker
 var notifier chan []byte
 
 const maxPacketSize = 1024 * 1024
 
+// formatMessage strips ANSI codes e.g. `[30m`
 func formatMessage(msg []byte) string {
 	re := regexp.MustCompile(`\[\d+m`)
 	msg = re.ReplaceAllLiteral(msg, []byte(""))
@@ -22,8 +24,13 @@ func formatMessage(msg []byte) string {
 	return string(msg)
 }
 
+// Global identifier to generate IDs for sequential messages
+// Currently in use to know we have unique objects on the frontend
 var messageID = 0
 
+// Process incoming udp payloads and send them as JSON to the frontend via
+// Server Sent Events
+// We add the current timestamp and a sequential messageID
 func processMessage(msg []byte) {
 	messageID++
 	fmt.Printf("%s", msg)
@@ -46,10 +53,21 @@ func processMessage(msg []byte) {
 
 	fmt.Println("output:", string(output))
 
+	// Send JSON payload to the SSE broker
 	notifier <- output
 }
 
-func listenToUDP(conn *net.UDPConn) {
+// listenToUDP will receive UDP payloads and process them
+func listenToUDP() {
+	addr, err := net.ResolveUDPAddr("udp", ":10001")
+	if err != nil {
+		log.Fatal("ResolveUDPAddr", err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatal("ListenUDP", err)
+	}
+
 	buffer := make([]byte, maxPacketSize)
 	for {
 		bytes, err := conn.Read(buffer)
@@ -64,18 +82,40 @@ func listenToUDP(conn *net.UDPConn) {
 	}
 }
 
+// Serve 3 static files currently in use, this is all we need right now so
+// lets not make it too complicated
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	switch path {
+	case "/":
+		http.ServeFile(w, r, "frontend/dist/index.html")
+	case "/styles.css":
+		http.ServeFile(w, r, "frontend/styles.css")
+	case "/app.js":
+		http.ServeFile(w, r, "frontend/dist/app.js")
+	}
+}
+
+// Setup everything
+// - Simple HTTP endpoint on port 8001 that responds to SSE connection requests
+// - UDP listener that will receive messages
+// - HTTP server that serves static files in use for the frontend
 func main() {
 	broker := NewServer()
 	notifier = broker.Notifier
+
+	// Start SSE endpoint that will register connections
 	go http.ListenAndServe(":8001", broker)
 
-	addr, err := net.ResolveUDPAddr("udp", ":10001")
+	// Start listening to UDP payloads
+	go listenToUDP()
+
+	http.HandleFunc("/", handleIndex)
+
+	fmt.Println("Will start listening on port 8000")
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
-		log.Fatal("ResolveUDPAddr", err)
+		log.Fatal("Could not start listening on port 8000")
 	}
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		log.Fatal("ListenUDP", err)
-	}
-	listenToUDP(conn)
 }
